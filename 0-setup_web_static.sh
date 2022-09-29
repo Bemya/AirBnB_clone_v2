b3743d69aa9ddb609ce2f06581151a4e4e8848ad
Bemya <99530208+Bemya@users.noreply.github.com>
2022-09-29 18:28:47 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
-#  sets up your web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-sudo apt-get -y install nginx
-mkdir -p /data
-mkdir /data/web_static
-mkdir /data/web_static/releases
-mkdir /data/web_static/shared
-mkdir /data/web_static/releases/test
-touch /data/web_static/releases/test/index.html
-echo "Deployment Test" > /data/web_static/releases/test/index.html
-rm -f /data/web_static/current && ln -s /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data
-redirect="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "50s|.*|$redirect|" /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
